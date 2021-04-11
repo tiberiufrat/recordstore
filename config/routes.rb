ef8e434181e3d56bd8b12d63018be7c00a896d83
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2021-04-11 18:02:13 +0300

    initialise app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :records
+      resources :artists
+    end
+  end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+end
---
+++ recordstore-front/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
+import Artists from '@/components/Artists'
+import Records from '@/components/Records'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/artists',
+      name: 'Artists',
+      component: Artists
+    },
+    {
+      path: '/records',
+      name: 'Records',
+      component: Records
+    },
+  ]
+})
